7a2f4cc628e149cfc193f0192a0fdcea896c931e
Daniel <sentret@outlook.com>
2019-01-16 20:06:49 -0300

    Add Phaser Hello World

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,27 @@
-console.log("Hello World");
+/// <reference path="../typings/phaser.d.ts"/>
+
+import { a } from "./test";
+
+class SimpleGame {
+    constructor() {
+        this.game = new Phaser.Game(a, a, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+    }
+
+    game: Phaser.Game;
+
+    preload() {
+        this.game.load.image('logo', 'cel.png');
+    }
+
+    create() {
+        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+    }
+
+}
+
+window.onload = () => {
+
+    var game = new SimpleGame();
+
+};
---
+++ src/test.ts
@@ -0,0 +1,1 @@
+export let a = (() => (console.log("a"), 1))();
